ef264d2c3fe5c3990cc267de4b9fadd30df8511b
Latika04Parolkar <latikaparolkar04@gmail.com>
2025-09-18 17:21:32 +0530

    added deployment needs

--- app.js
+++ app.js
@@ -11,7 +11,9 @@ app.use(express.json());
 
 app.use("/chat", chatRoutes);
 
-const PORT = process.env.PORT || 3001;
-app.listen(PORT, () => {
-  console.log(`🚀 Server running on http://localhost:${PORT}`);
-});
+// const PORT = process.env.PORT || 3001;
+// app.listen(PORT, () => {
+//   console.log(`🚀 Server running on http://localhost:${PORT}`);
+// });
+
+export default app;
